f4a3e5afb9f00f083dfdcc53b273574b5814da12
Lars Verhaeghe <148888518+larsv2801@users.noreply.github.com>
2025-03-24 19:55:15 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/F1/index.html',
+        '/F1/manifest.json',
+        '/F1/voorlopiglogo192.png',
+        '/F1/voorlopiglogo144.png',
+        '/F1/voorlopiglogo.png',
+        'F1/Formula1-Bold.otf'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
